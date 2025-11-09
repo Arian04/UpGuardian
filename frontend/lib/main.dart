8a6c20f062fc0a7fa75daa859dc35f9a4608fb02
kitkashi <skit.edwards@gmail.com>
2025-11-09 03:43:47 -0600

    create seperate tab bar

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:buttons_tabbar/buttons_tabbar.dart';
 import 'package:flutter/material.dart';
 import 'package:up_guardian/requests_page.dart';
 import 'package:up_guardian/rules_page.dart';
+import 'package:up_guardian/tab_bar_example_page.dart';
 import 'package:up_guardian/tests_page.dart';
 
 void main() {
@@ -24,54 +25,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class TabBarExamplePage extends StatefulWidget {
-  const TabBarExamplePage({super.key});
-
-  @override
-  State<TabBarExamplePage> createState() => _TabBarExamplePageState();
-}
-
-class _TabBarExamplePageState extends State<TabBarExamplePage> {
-  @override
-  Widget build(BuildContext contect) {
-    return DefaultTabController(
-      initialIndex: 1,
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text("TabBar"),
-          bottom: ButtonsTabBar(
-          backgroundColor: Colors.black, 
-          unselectedBackgroundColor:Colors.grey[400],
-          unselectedLabelStyle: const TextStyle(color: Colors.white),
-          labelStyle: const TextStyle(color: Colors.white,fontWeight: FontWeight.bold),
-          tabs:const [
-            Tab(
-              icon: Icon(Icons.directions_bike),
-            text:"Requests",),
-            Tab(
-              icon: Icon(Icons.lock_clock_sharp),
-            text:"Test",),
-            Tab(
-              icon: Icon(Icons.lock_clock_sharp),
-            text:"Rules",)
-          ],
-          ),
-          ),
-          body:const TabBarView(
-          children:[
-            Center(
-            child: RequestsPage(),
-          ),
-           Center(
-            child:  TestsPage(),
-           ),
-          Center(
-            child: RulesPage(),
-          ),
-          ],
-          )
-        ),
-      );
-  }
-}
