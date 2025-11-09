6b9fec98435d74044f18f2b3d6bf969cffcbcd79
kitkashi <skit.edwards@gmail.com>
2025-11-09 04:01:38 -0600

    create stateful widget

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,10 +1,5 @@
-import 'package:buttons_tabbar/buttons_tabbar.dart';
 import 'package:flutter/material.dart';
 import 'package:up_guardian/my_home_page.dart';
-import 'package:up_guardian/requests_page.dart';
-import 'package:up_guardian/rules_page.dart';
-import 'package:up_guardian/tab_bar_example_page.dart';
-import 'package:up_guardian/tests_page.dart';
 
 void main() {
   runApp(const MyApp());
