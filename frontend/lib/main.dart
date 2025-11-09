b372f5fd031f63384e6ca8bc7d1acf80c28bc517
kitkashi <skit.edwards@gmail.com>
2025-11-08 21:28:05 -0600

    fix tabs for services page

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:buttons_tabbar/buttons_tabbar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'UpGuardian',
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed
+        (seedColor: Colors.black) ,
+        useMaterial3: true),
       home: const TabBarExamplePage(),
     );
   }
@@ -34,9 +38,9 @@ class _TabBarExamplePageState extends State<TabBarExamplePage> {
         appBar: AppBar(
           title: const Text("TabBar"),
           bottom: ButtonsTabBar(
-          backgroundColor: Colors.white, 
-          unselectedBackgroundColor:Colors.grey,
-          unselectedLabelStyle: const TextStyle(color: Colors.black),
+          backgroundColor: Colors.black, 
+          unselectedBackgroundColor:Colors.grey[400],
+          unselectedLabelStyle: const TextStyle(color: Colors.white),
           labelStyle: const TextStyle(color: Colors.white,fontWeight: FontWeight.bold),
           tabs:const [
             Tab(
@@ -47,12 +51,11 @@ class _TabBarExamplePageState extends State<TabBarExamplePage> {
             text:"Test",),
             Tab(
               icon: Icon(Icons.lock_clock_sharp),
-            text:"Rules",),
+            text:"Rules",)
           ],
           ),
           ),
-        ),
-        body:const TabBarView(
+          body:const TabBarView(
           children:[
             Center(
             child:  Text("User"),
@@ -64,7 +67,8 @@ class _TabBarExamplePageState extends State<TabBarExamplePage> {
             child: Text("User"),
           ),
           ],
-          ),
+          )
+        ),
       );
   }
 }
