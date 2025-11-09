8fdd18341c756cdf9f567eaf1165b5fdb7a34398
Saran <saran.innovations@gmail.com>
2025-11-08 22:52:31 -0600

    add text input

--- frontend/lib/rules_page.dart
+++ frontend/lib/rules_page.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+final TextEditingController _controller = TextEditingController();
 
 class RulesPage extends StatelessWidget {
-  const RulesPage({
-    super.key,
-  });
-
+  const RulesPage({super.key});
+  
   @override
   Widget build(BuildContext context) {
-    return Text("Rules");
+    return TextField(
+      controller: _controller,
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Custom Rules',
+      ),
+    );
   }
 }
